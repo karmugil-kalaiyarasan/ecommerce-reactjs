82d98fdc464fc617a6c960275e4aee1c90f92f23
karmugil-kalaiyarasan <63269889+karmugil-kalaiyarasan@users.noreply.github.com>
2022-02-03 17:11:50 -0500

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
+import Category from "../components/Category";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Announcement />
+      <Slider />
+      <Category />
+      <Products />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
